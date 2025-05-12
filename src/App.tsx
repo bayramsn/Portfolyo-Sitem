b6f69acf7bce9c9d0aaa67c087f4807d16cb050c
bayramsn <116954592+bayramsn@users.noreply.github.com>
2025-05-13 01:34:57 +0300

    GitHub Pages SPA sorununu düzeltmek için değişiklikler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
