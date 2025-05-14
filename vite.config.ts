fbdebe018f9e603634ff900e25cb4aa229fe643a
bayramsn <116954592+bayramsn@users.noreply.github.com>
2025-05-14 17:44:04 +0300

    Vite ve HTML konfigürasyonu düzeltildi

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,10 +8,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
   }
 })
